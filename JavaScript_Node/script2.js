2315979e98ed7a382e4e8c301fb5941c5098e486
Oxana Bulanova <oxana_bulanova@hotmail.com>
2021-12-08 22:12:50 +0100

    bouncer at the door
    if else statements with multiple simultaneous conditions.

---
+++ JavaScript_Node/script2.js
@@ -0,0 +1,40 @@
+const age = 25;
+const isFemale = false;
+const driverStatus = 'drunk';
+
+if (age >= 18) {
+    console.log('You are allowed in!');
+    
+    if (isFemale == true){
+        console.log("Free drinks for ladies tonight!");
+    } else {
+        console.log("No free drinks for you man!");
+    }
+
+    if (driverStatus== 'bob') {
+        console.log('You are allowed to drive');
+    } else {
+        console.log('You are not allowed to drive');
+    }
+
+} else {
+    console.log('You are too young, please stay outside');
+}
+
+// if (isFemale == true) {
+//     console.log("Free drinks for ladies tonight!");
+// } else {
+//     console.log("No free drinks for you man!");
+// }
+
+// if (driverStatus== 'bob') {
+//     console.log('You are allowed to drive');
+// } else {
+//     console.log('You are not allowed to drive');
+// }
+
+
+
+
+
+
