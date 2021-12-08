927803c4dc2180a76b141890d1f0f2e79c7acda6
Oxana Bulanova <oxana_bulanova@hotmail.com>
2021-12-08 15:28:57 +0100

    javaScript lesson 1
    basic variable types and creating <script>

---
+++ 1-JavaScript/myScript.js
@@ -0,0 +1,22 @@
+//4.
+console.log("Hello Winc Academy")
+//5.
+let myName = 'Oxana';
+console.log(myName);
+//6. if is a reserved word
+//7.
+console.log(4 + 4); // result is 8
+console.log("4" + "4"); // result is 44
+//8.
+let x = 10;
+let y = 5;
+console.log(x + y); //15
+console.log(x - y); //5
+console.log(x * y); //50
+console.log(x / y); //2
+console.log(x % y); //0, there is no rest
+//9.
+let myAge = 35;
+console.log(typeof(myAge)); //number
+myAge = "35";
+console.log(typeof(myAge)); //string
