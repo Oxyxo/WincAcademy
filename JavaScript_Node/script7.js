44e6323da99bc47c06d943fd4dd491f59700174f
Oxana Bulanova <oxana_bulanova@hotmail.com>
2021-12-12 18:30:13 +0100

    some solutions

--- JavaScript_Node/script7.js
+++ JavaScript_Node/script7.js
@@ -58,20 +58,3 @@ const square = number => {
 
 const add2 = (number1, number2) => number1 + number2;
 
-
-function calculation(number1, number2) {
-    calc1 = number1*number1 + number2*number2
-    calc2 = calc1*calc1
-    return calc2;
-}
-console.log(calculation(10, 5)); //15625
-
-const calculation2 = function(number1, number2) {
-    calc1 = number1*number1 + number2*number2
-    calc2 = calc1*calc1
-    return calc2;
-}
-console.log(calculation2(10, 5)); //15625
-
-const calculation3 = (number1, number2) => (number1*number1 + number2*number2)*(number1*number1 + number2*number2);
-console.log(calculation3(10, 5)); //15625
--- JavaScript_Node/script8.js
+++ JavaScript_Node/script8.js
@@ -1,16 +1,27 @@
 function calculation(number1, number2) {
-    calc1 = number1*number1 + number2*number2
-    calc2 = calc1*calc1
+    const calc1 = number1*number1 + number2*number2
+    const calc2 = calc1*calc1
     return calc2;
 }
 console.log(calculation(10, 5)); //15625
 
 const calculation2 = function(number1, number2) {
-    calc1 = number1*number1 + number2*number2
-    calc2 = calc1*calc1
+    const calc1 = number1*number1 + number2*number2
+    const calc2 = calc1*calc1
     return calc2;
 }
 console.log(calculation2(10, 5)); //15625
 
 const calculation3 = (number1, number2) => (number1*number1 + number2*number2)*(number1*number1 + number2*number2);
 console.log(calculation3(10, 5)); //15625
+
+//shorter solutions
+const doSquareCalculation = function(number1, number2) {
+    const sum = number1 * number1 + number2 * number2;
+    return sum * sum;
+};
+
+const square = number => number * number;
+
+const doSquareCalculation = (number1, number2) =>
+    square(square(number1) + square(number2));
