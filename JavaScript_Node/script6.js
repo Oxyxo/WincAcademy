062baa14765f9914e0ebdb7806a1c979f74a9a7c
Oxana Bulanova <oxana_bulanova@hotmail.com>
2021-12-12 16:47:36 +0100

    function arguments JS
    arguments JS

---
+++ JavaScript_Node/script6.js
@@ -0,0 +1,16 @@
+function wallPainter(color, side){
+    console.log("The " + side + " wall has been painted " + color);
+};
+wallPainter(); //The wall has been painted undefined
+
+wallPainter('orange', 'north');//The north wall has been painted orange
+wallPainter('grey', 'south-east');//The south-east wall has been painted grey
+
+//does the order of the arguments matter when calling the function?
+//what happens if you switch them in the function definition? ->nothing
+
+//what happens if you change the order of the arguments in the function
+//definition and when calling the function?
+
+wallPainter('south-east', 'grey'); //The grey wall has been painted south-east
+//if you give wrong order when calling a function it will put it in wrong places
