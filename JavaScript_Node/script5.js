d9d4393e963287d86767f8a88a4ecf9bc352655b
Oxana Bulanova <oxana_bulanova@hotmail.com>
2021-12-12 16:31:29 +0100

    introduction to functions
    functions JS

---
+++ JavaScript_Node/script5.js
@@ -0,0 +1,27 @@
+//functions
+
+const makePizza = function () {
+    console.log("prepare sauce");
+    console.log("prepare topping");
+    console.log("mix ingredients together");
+    console.log("bake the piza in the ovn");
+
+};
+
+//take order, prepare sushi, serve/pack
+const makeSushi = function() {
+    console.log("prepare nori");
+    console.log("add rice to nori");
+    console.log("add toppings and sauce on the rice");
+    console.log("roll the sushi");
+
+};
+
+// change your code so that the output of your program is the steps for making sushi, three times and pizza two times
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
+
